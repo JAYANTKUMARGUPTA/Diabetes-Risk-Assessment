43bc2242e3f2db3682bbb6e70cce9c1b20a6e924
Jayant gupta <jayantkumargupta41l@example.com>
2025-04-21 10:15:59 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -64,6 +64,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!", error: err.message });
 });
 
+app.get('/',(req,res)=>{
+  res.redirect(process.env.CLIENT_URL)
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
